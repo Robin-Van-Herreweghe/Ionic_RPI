de783b207893e12b31c60d2c5ae06481901a305c
Robin-Van-Herreweghe <robinvh1403@gmail.com>
2020-12-01 12:45:53 +0100

    Final-Commitv2

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -10,35 +10,35 @@ const Tab2: React.FC = () => {
   const [checked, setChecked] = useState(false);
   const [checked2, setChecked2] = useState(false);
   let mqttClient = mqttConnect("wss://mqtt.wdaan.tools")
-  //export MQTT
+  
   mqttClient.on("connect", function () {
-    // subscribe on a topic after connected
+    
     mqttClient.subscribe("PiRobin")
-    // send a message to test if the connection is working
-   // mqttClient.publish("PiRobin", "Connected")
+    
+   
   })
   mqttClient.on("message", function (topic, message) {
-    // show received message
+    
     console.log(topic, message.toString())
     if(message.toString() == "pin 33 is laag"){
-      // achtergrond van de button veranderen.
+      
       setChecked(false)
       console.log("checked low");
     }
     else if(message.toString() == "Pin 33 is hoog"){
-      // achtergrond van de button veranderen.
+      
       setChecked(true)
       console.log("checked high");
     }
 
 
     if(message.toString() == "pin 31 is laag"){
-      // achtergrond van de button veranderen.
+      
       setChecked2(false)
       console.log("checked P2 low");
     }
     else if(message.toString() == "Pin 31 is hoog"){
-      // achtergrond van de button veranderen.
+      
       setChecked2(true)
       console.log("checked P2 high");
     }
